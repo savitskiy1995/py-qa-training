3fcf6bd2a155e906dddcbb66b7a1591a11f4cc33
savitskiy1995 <glebsav@bk.ru>
2025-05-13 10:36:51 +0300

    Задание 9

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
 
+from model import contact
+from model.contact import Contact
+
 
 class ContactHelper:
 
     def __init__(self, app):
         self.app = app
+
 
     def create_contact(self, contact):
         wd = self.app.wd
@@ -15,12 +19,14 @@ class ContactHelper:
         self.app.wait_for_element(By.XPATH, "//div[@id='content']/form/input[20]").click()
         self.return_to_home_page()
 
+
     def fill_contact_form(self, contact):
         self.app.wait_for_element(By.NAME, "firstname").send_keys(contact.firstname)
         self.app.wait_for_element(By.NAME, "lastname").send_keys(contact.lastname)
         self.app.wait_for_element(By.NAME, "company").send_keys(contact.company)
         self.app.wait_for_element(By.NAME, "home").send_keys(contact.home_phone)
         self.app.wait_for_element(By.NAME, "email").send_keys(contact.email)
+
 
     def open_add_contact_page(self):
         self.app.wait_for_element(By.LINK_TEXT, "add new").click()
@@ -32,11 +38,28 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
+        if not self.is_contact_exist():
+            self.open_add_contact_page()
+            self.create_contact(Contact(
+                firstname="John",
+                lastname="Smith",
+                company="Google",
+                home_phone="+7999999999",
+                email="johnsmith@gmail.com"
+            ))
         self.app.wait_for_element(By.NAME, "selected[]").click()
         self.app.wait_for_element(By.XPATH, "//input[@value='Delete']").click()
 
 
     def edit_first_contact(self, contact):
         wd = self.app.wd
+        if not self.is_contact_exist():
+            self.open_add_contact_page()
+            self.create_contact(contact)
         self.app.wait_for_element(By.XPATH, "//img[@title='Edit']").click()
         self.fill_contact_form(contact)
+
+
+    def is_contact_exist(self):
+        wd = self.app.wd
+        return len(wd.find_elements(By.NAME, "selected[]")) > 0
