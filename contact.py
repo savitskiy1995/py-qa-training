fc062549f619101dcbb56d18563e077b01bc6d04
savitskiy1995 <glebsav@bk.ru>
2025-05-13 10:29:05 +0300

    Delete contact.py

--- contact.py
+++
@@ -1,7 +0,0 @@
-class Contact:
-    def __init__(self, firstname, lastname, company, home_phone, email):
-        self.firstname = firstname,
-        self.lastname = lastname,
-        self.company = company,
-        self.home_phone = home_phone,
-        self.email = email
