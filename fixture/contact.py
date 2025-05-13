4e9e266e60587146626d94e66677f4cdffe7d08e
savitskiy1995 <glebsav@bk.ru>
2025-05-13 10:49:39 +0300

    Задание 14

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,3 +1,5 @@
+import re
+
 from selenium.webdriver.common.by import By
 
 from model.contact import Contact
@@ -78,13 +80,15 @@ class ContactHelper:
                 home_phone="+7999999999",
                 email="johnsmith@gmail.com"
             ))
-        self.select_contact_by_index(index)
+        self.open_edit_contact_by_index(index)
         self.app.wait_for_element(By.XPATH, "//input[@value='Delete']").click()
         self.contacts_cache = None
 
 
-    def select_contact_by_index(self, index):
+    def open_edit_contact_by_index(self, index):
         wd = self.app.wd
+        if not wd.current_url.endswith("addressbook/"):
+            wd.find_element(By.XPATH, "//a[contains(.,'home')]").click()
         wd.find_elements(By.XPATH, "//img[@title='Edit']")[index].click()
 
 
@@ -98,7 +102,7 @@ class ContactHelper:
         if not self.is_contact_exist():
             self.open_add_contact_page()
             self.create_contact(contact)
-        self.select_contact_by_index(index)
+        self.open_edit_contact_by_index(index)
         self.fill_contact_form(contact)
         self.app.wait_for_element(By.XPATH, "//input[@value='Update']").click()
         self.contacts_cache = None
@@ -121,8 +125,68 @@ class ContactHelper:
                 first_name = cells[2].text
                 address = cells[3].text
                 email = cells[4].text
-                phone = cells[5].text
-                self.contacts_cache.append(Contact(id=id, firstname=first_name, lastname=last_name,
-                                                  address=address, email=email,
-                                                  home_phone=phone))
+                all_phones = cells[5].text
+
+                email = email if email else None
+                all_phones = all_phones if all_phones else None
+
+                self.contacts_cache.append(Contact(
+                    id=id,
+                    firstname=first_name,
+                    lastname=last_name,
+                    address=address,
+                    all_emails_from_home_page=email,
+                    all_phones_from_home_page=all_phones
+                ))
         return list(self.contacts_cache)
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_edit_contact_by_index(index)
+        firstname = wd.find_element(By.NAME, "firstname").get_attribute("value")
+        lastname = wd.find_element(By.NAME, "lastname").get_attribute("value")
+        id = wd.find_element(By.NAME, "id").get_attribute("value")
+        homephone = wd.find_element(By.NAME, "home").get_attribute("value")
+        workphone = wd.find_element(By.NAME, "work").get_attribute("value")
+        mobilephone = wd.find_element(By.NAME, "mobile").get_attribute("value")
+        address = wd.find_element(By.NAME,"address").get_attribute("value")
+        email = wd.find_element(By.NAME,"email").get_attribute("value")
+        email2 = wd.find_element(By.NAME,"email2").get_attribute("value")
+        email3 = wd.find_element(By.NAME,"email3").get_attribute("value")
+        return Contact (firstname=firstname, lastname=lastname, id=id, home_phone=homephone,
+                        workphone=workphone, mobilephone=mobilephone,
+                        address=address, email=email, email2=email2, email3=email3)
+
+
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements(By.NAME, "entry")[index]
+        cell = row.find_elements(By.TAG_NAME, "td")[6]
+        cell.find_element(By.TAG_NAME, "a").click()
+    
+    
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element(By.ID, "content").text
+        # Ищем номера, но если не находим — возвращаем пустую строку
+        homephone_match = re.search(r"H: (.*)", text)
+        workphone_match = re.search(r"W: (.*)", text)
+        mobilephone_match = re.search(r"M: (.*)", text)
+
+        homephone = homephone_match.group(1) if homephone_match else ""
+        workphone = workphone_match.group(1) if workphone_match else ""
+        mobilephone = mobilephone_match.group(1) if mobilephone_match else ""
+
+        return Contact(
+            home_phone=homephone,
+            workphone=workphone,
+            mobilephone=mobilephone
+        )
+
+    def open_home_page(self):
+        wd = self.app.wd
+        if not wd.current_url.endswith("addressbook/"):
+            wd.find_element(By.XPATH, "//a[contains(.,'home')]").click()
--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,10 @@ from sys import maxsize
 
 
 class Contact:
-    def __init__(self, firstname=None, lastname=None, address=None, home_phone=None, company = None, email=None, id=None):
+    def __init__(self, firstname=None, lastname=None, address=None, home_phone=None,
+                 company = None, email=None, id=None, all_phones_from_home_page = None,
+                 mobilephone=None, secondaryphone=None, workphone=None, all_emails_from_home_page = None,
+                 email2 = None, email3 = None):
         self.firstname = firstname
         self.lastname = lastname
         self.company = company
@@ -10,6 +13,13 @@ class Contact:
         self.home_phone = home_phone
         self.email = email
         self.id = id
+        self.mobilephone = mobilephone
+        self.secondaryphone = secondaryphone
+        self.workphone = workphone
+        self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_home_page = all_emails_from_home_page
+        self.email2 = email2
+        self.email3 = email3
 
 
     def __repr__(self):
