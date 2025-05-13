19e20178da8c12f4521a5c5a7566d376fe613f0b
savitskiy1995 <glebsav@bk.ru>
2025-05-13 10:11:35 +0300

    Задание 2

--- test_add_group.py
+++ test_add_group.py
@@ -5,8 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
-
+import unittest
+from group import Group
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -23,24 +23,52 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        self.open_home_page(wd)
+        self.login(username="admin", password="secret")
+        self.open_group_page()
+        self.create_group(Group(name="new_group", header="logo"))
+        self.return_to_groups_page()
+        self.logout()
 
-        self.wait_for_element(By.NAME, "user").send_keys("admin")
-        self.wait_for_element(By.NAME, "pass").send_keys("secret")
-        self.wait_for_element(By.XPATH, "//input[@value='Login']").click()
-        self.wait_for_element(By.LINK_TEXT, "groups").click()
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(username="admin", password="secret")
+        self.open_group_page()
+        self.create_group(Group(name="", header=""))
+        self.return_to_groups_page()
+        self.logout()
+
+    def logout(self):
+        self.wait_for_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_groups_page(self):
+        self.wait_for_element(By.LINK_TEXT, "group page").click()
+
+    def create_group(self, group):
+        # init group creation
         self.wait_for_element(By.NAME, "new").click()
+        # fill group form
         group_name = self.wait_for_element(By.NAME, "group_name")
         group_name.click()
-        group_name.clear()
-        group_name.send_keys("new_group")
+        group_name.send_keys(group.name)
         group_header = self.wait_for_element(By.NAME, "group_header")
         group_header.click()
         group_header.clear()
-        group_header.send_keys("logo")
+        group_header.send_keys(group.header)
+        # submit group creation
         self.wait_for_element(By.NAME, "submit").click()
-        self.wait_for_element(By.LINK_TEXT, "group page").click()
-        self.wait_for_element(By.LINK_TEXT, "Logout").click()
+
+    def open_group_page(self):
+        self.wait_for_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, username, password):
+        self.wait_for_element(By.NAME, "user").send_keys(username)
+        self.wait_for_element(By.NAME, "pass").send_keys(password)
+        self.wait_for_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
@@ -58,7 +86,6 @@ class TestAddGroup(unittest.TestCase):
 
     def tearDown(self):
         self.wd.quit()
-
 
 if __name__ == "__main__":
     unittest.main()
